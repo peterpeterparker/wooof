dfd294faebdc65f93f705c3d048e95e795c84c9a
peterpeterparker <david.dalbusco@outlook.com>
2019-11-28 14:09:16 +0100

    feat: don't reimport workbox

--- public/service-worker-cache.js
+++ public/service-worker-cache.js
@@ -1,5 +1,3 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 workbox.routing.registerRoute(
     // Cache Image File
     new RegExp(/.*\.(?:png|jpg|jpeg|svg|gif)/),
